c9a5aeb7075e0b6cf2b030ae89d5256b05fc35fe
Daniel Ulises Chavez Morales <daulisescm@gmail.com>
2026-02-12 19:32:29 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-ccd32a7a-ad62-4df2-b394-3696b4c9b59f.containerhub.tripleten-services.com"
+DOC_PATH = "https://cnt-ccd32a7a-ad62-4df2-b394-3696b4c9b59f.containerhub.tripleten-services.com/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
